21b8f2b3d250ba957034cce49401fc39a0d9b399
Fuad Mubarak <fuad.mubarak@inteleq.id>
2026-05-11 21:23:16 +0700

    Add looping background music

--- src/App.js
+++ src/App.js
@@ -28,17 +28,44 @@
 
 
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "./App.css";
 import { initSpaceScene } from "./spaceScene";
+import bgMusic from "../src/assets/jl.mp3";
 
 function App() {
+  const audioRef = useRef(null);
+
   useEffect(() => {
     initSpaceScene();
+
+    const playMusic = async () => {
+      try {
+        audioRef.current.volume = 0.02; // 10% volume
+        await audioRef.current.play();
+      } catch (err) {
+        console.log("Autoplay blocked");
+      }
+    };
+
+    playMusic();
+
+    // browser fallback
+    window.addEventListener("click", playMusic);
+
+    return () => {
+      window.removeEventListener("click", playMusic);
+    };
   }, []);
 
   return (
     <div id="wrap">
+
+      {/* Background Music */}
+      <audio ref={audioRef} loop hidden>
+        <source src={bgMusic} type="audio/mp3" />
+      </audio>
+      
       <canvas id="c"></canvas>
 
       <div id="cur"></div>
